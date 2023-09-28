8f9a36fa0c4c03a30081f76a846dadc077818611
idogbo2023 <paul.adigwu@techconsulting.tech>
2023-09-28 01:59:01 +0000

    testing.sh

--- testing.sh
+++ testing.sh
@@ -7,10 +7,12 @@ do
   cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
   memUsage=$(free -m | awk '/Mem/{print $3}')
   diskUsage=$(df -h | awk '$NF=="/"{printf " %d/%dGB (%s)\n", $3,$2,$5}')
+  netWork=$(iftop -t -s 2 | awk '/Mem/{print $2}')
   # Print the usage
   echo "CPU Usage: $cpuUsage%"
   echo "Memory Usage: $memUsage MB"
   echo "disk Usage: $diskUsage "
+  echo "network: $netWork "
 
   # Sleep for 1 second
   sleep 1
