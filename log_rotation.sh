fc18dcab1a4a33092828ff5ce9547ae0f02e1fab
idogbo2023 <146276888+idogbo2023@users.noreply.github.com>
2023-09-28 22:10:14 -0400

    Update log_rotation.sh

--- log_rotation.sh
+++ log_rotation.sh
@@ -1,34 +1,37 @@
+# this line is called a shebang, it specifies the path to the Bash interpreter
 #!/bin/bash
 
-# Configuration
-log_directory="/var/log/httpd" # Change this to your web server's log directory
-backup_directory="/backup/logs" # Change this to your desired backup directory
-retention_days=7 # Number of days to retain log backups
+# This lines define configuration variables for the script. log_directory specifies the directory where your web server's log files are located. backup_directory
+ specifies the directory where backup logs will be stored, retention_days sets the number of days to retain log backups
+log_directory="/var/log/httpd"
+backup_directory="/backup/logs"
+retention_days=7
 
-# Date format for log files
+# DCurrent date in format of yyyy-mm-dd and stores in date_format variable
 date_format=$(date +"%Y-%m-%d")
 
-# Check if the log directory exists
+# Check if the specified log directory exists. if it does not, it prints an error message and exit the script with a status code of 1
 if [ ! -d "$log_directory" ]; then
     echo "Log directory '$log_directory' does not exist."
     exit 1
 fi
 
-# Create the backup directory if it doesn't exist
+# Create if the backup directory exist. if it doesn't, it creates the directory using mkdir
 if [ ! -d "$backup_directory" ]; then
     mkdir -p "$backup_directory"
 fi
 
-# Rotate and compress logs
+# Uses the find command to locate all files with a log extension in the specified directory, it then uses the -exec option to pass each found file to the gzip -9
+command, compressing the log files to maximum
 find "$log_directory" -type f -name "*.log" -exec gzip -9 {} \;
 
-# Move rotated logs to the backup directory
+# Move all compressed log.gz files from log to backup directory
 mv "$log_directory"/*.log.gz "$backup_directory/"
 
 # Delete log backups older than the retention period
 find "$backup_directory" -type f -name "*.log.gz" -mtime +$retention_days -delete
 
 # Optional: Restart your web server to create new log files
-# systemctl restart httpd # Uncomment this line if necessary
+# systemctl restart httpd
 
 echo "Log rotation and backup completed."
