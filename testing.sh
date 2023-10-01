ae181f854994257b76dd81c137d5f30f65494904
idogbo2023 <146276888+idogbo2023@users.noreply.github.com>
2023-10-01 19:09:07 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -19,7 +19,7 @@ MEMORY_UTILIZATION=$(free | grep Mem | awk '{print $3/$2 * 100.0}' | cut -d. -f1
 DISK_UTILIZATION=$(df -h / | awk 'NR==2{print $5}' | cut -d% -f1)
 
 # Monitor Network utilization (assuming eth0 interface)
-NETWORK_UTILIZATION=$(ifstat -n -q 1 1 | awk '{print $1}' | tail -n 1)
+NETWORK_UTILIZATION=$(ifstat -n -q 1 1 | awk '{print int($1)}' | tail -n 1)
 
 # Check if thresholds are exceeded and log to Metrics.txt
 if [ "$CPU_UTILIZATION" -gt "$CPU_THRESHOLD" ] || [ "$MEMORY_UTILIZATION" -gt "$MEMORY_THRESHOLD" ] || [ "$DISK_UTILIZATION" -gt "$DISK_THRESHOLD" ] || [ "$NETWORK_UTILIZATION" -gt "$NETWORK_THRESHOLD" ]; then
