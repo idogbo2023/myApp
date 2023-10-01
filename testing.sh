740d3a3c5b0e128df54bf39dd24f236a1a2cf197
idogbo2023 <146276888+idogbo2023@users.noreply.github.com>
2023-10-01 17:03:57 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-cpu_threshold=90
-mem_threshold=900
-disk_threshold=90
-netwrk_threshold=1000
+# Define thresholds (adjust these values as needed)
+CPU_THRESHOLD=80
+MEMORY_THRESHOLD=90
+DISK_THRESHOLD=90
+NETWORK_THRESHOLD=1000 # You can set the threshold in KB/s
 
-  # Get the current usage of CPU and memory
-  cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
-  memUsage=$(free -m | awk '/Mem/{print $3}')
-  diskUsage=$(df -h | awk '$NF=="/"{printf " %d/%dGB (%s)\n", $3,$2,$5}')
-  netWork=$(ifstat -n -q 1 1 | awk '{print $1}' | tail -n 1)
-  
-  #check if thresholds are exceeeded
-  # if [ "$cpuUsage" -gt "$cpu_threshold" ] || [ "$memUsage" -gt "$mem_threshold" ] || [ "$diskUsage" -gt "$disk_threshold" ] || [ "$netWork" -gt "$netwrk_threshold" ]; 
-# then
+# Get current date and time
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-  # Print the usage
-  echo "CPU Usage: $cpuUsage%"
-  echo "Memory Usage: $memUsage MB"
-  echo "disk Usage: $diskUsage "
-  echo "Network Usage: $netWork "
-  echo "{Cpu $cpuUsage},{Memory $memUsage},{Network $netWork},{Disk $diskUsage}" > Metrics.txt
-  # fi 
+# Monitor CPU utilization
+CPU_UTILIZATION=$(top -b -n 1 | grep "%Cpu(s)" | awk '{print $2}' | cut -d. -f1)
+
+# Monitor Memory utilization
+MEMORY_UTILIZATION=$(free | grep Mem | awk '{print $3/$2 * 100.0}' | cut -d. -f1)
+
+# Monitor Disk utilization
+DISK_UTILIZATION=$(df -h / | awk 'NR==2{print $5}' | cut -d% -f1)
+
+# Monitor Network utilization (assuming eth0 interface)
+NETWORK_UTILIZATION=$(ifstat -n -q 1 1 | awk '{print $1}' | tail -n 1)
+
+# Check if thresholds are exceeded and log to Metrics.txt
+if [ "$CPU_UTILIZATION" -gt "$CPU_THRESHOLD" ] || [ "$MEMORY_UTILIZATION" -gt "$MEMORY_THRESHOLD" ] || [ "$DISK_UTILIZATION" -gt "$DISK_THRESHOLD" ] || [ "$NETWORK_UTILIZATION" -gt "$NETWORK_THRESHOLD" ]; then
+    echo "Timestamp: $TIMESTAMP" >> /var/log/Metrics.txt
+    echo "CPU Utilization: $CPU_UTILIZATION%" >> /var/log/Metrics.txt
+    echo "Memory Utilization: $MEMORY_UTILIZATION%" >> /var/log/Metrics.txt
+    echo "Disk Utilization: $DISK_UTILIZATION%" >> /var/log/Metrics.txt
+    echo "Network Utilization: $NETWORK_UTILIZATION KB/s" >> /var/log/Metrics.txt
+    echo "-----------------------------------------" >> /var/log/Metrics.txt
+fi
